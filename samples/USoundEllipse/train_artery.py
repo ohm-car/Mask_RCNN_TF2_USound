ac097f4d81558862b9bdc247df61ddd9700faa66
omkar-umbc <omkark1@umbc.edu>
2022-08-03 23:18:14 -0400

    Resnet50

--- samples/USoundEllipse/train_artery.py
+++ samples/USoundEllipse/train_artery.py
@@ -67,7 +67,7 @@ class ArteryConfig(Config):
 
     # We use a GPU with 12GB memory, which can fit two images.
     # Adjust down if you use a smaller GPU.
-    IMAGES_PER_GPU = 24
+    IMAGES_PER_GPU = 12
 
     # Number of classes (including background)
     NUM_CLASSES = 1 + 1  # Background + balloon
